40ba12480d5b3a72a450a2e85132bc1bd73fa339
FernandoWelzel <66530874+FernandoWelzel@users.noreply.github.com>
2021-04-06 22:51:28 -0300

    Name menu
    Adição de um menu para o usuário inserir um nome de até 8 caracteres.

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "raylib.h"
 #define MAX_FONTS   1
+#define MAX_INPUT_CHARS     8
 
 // Estruturas
 typedef struct gravacao {
@@ -14,8 +15,8 @@ typedef struct gravacao {
 } GRAVACAO;
 
 typedef struct fase {
-    char tamanhox;
-    char tamanhoy;
+    char estrutura[11][11];
+    int pos_porta;
 } FASE;
 
 typedef struct ponto {
@@ -25,23 +26,34 @@ typedef struct ponto {
 
 typedef struct status {
     char parte[5];
-}STATUS;
+} STATUS;
 
-typedef struct lolo{
+typedef struct lolo {
     char vidas;
     PONTO ponto;
-    int poder[3];
-    
-} Lolo;
+    char poder[3];
+} LOLO;
 
+typedef struct inimigo {
+    char vidas;
+    PONTO ponto;
+} INIMIGO;
+
+bool IsAnyKeyPressed()
+{
+    bool keyPressed = false;
+    int key = GetKeyPressed();
+
+    if ((key >= 32) && (key <= 126)) keyPressed = true;
+
+    return keyPressed;
+}
 
 int main(void)
-{
+{    
+    // Inicia a janela com as dimensões indicadas
     int screen_width = 800;
-    int screen_height = 600;
-    
-    STATUS status_jogo = {"MENU"};
-
+    int screen_height = 800;
     InitWindow(screen_width, screen_height, "Adventures of Lolo");
 
     //Iniciar audio device
@@ -51,8 +63,9 @@ int main(void)
     const char message[128] = "PRESS ENTER TO RETURN MENU";
     int framesCounter = 0;
     
-    
+    // Configurações
     SetTargetFPS(60);
+    STATUS status_jogo = {"MENU"};
     
     //Musica
     Music music = LoadMusicStream("./resources/Songs/Main-theme.mp3");
@@ -60,26 +73,30 @@ int main(void)
     PlayMusicStream(music);
     
     //Fonte
-     Font Fonte_principal = LoadFont("./resources/fonte.ttf");
+    Font Fonte_principal = LoadFont("./resources/fonte.ttf");
 
-
-    
-    
+    //Texturas
     Texture2D menu_texture = LoadTexture("./resources/Menus/Menu.png");
     Texture2D lolo_texture = LoadTexture("./resources/Menus/Menu_lolo.png"); 
     Texture2D cred_texture = LoadTexture("./resources/Menus/Creditos.png"); 
-    Texture2D arvore_texture = LoadTexture("./resources/Blocos/Tree.png");
     Texture2D fundo_texture = LoadTexture("./resources/Menus/Fundo sem nada.png");
+    Texture2D mapa_vazio_texture = LoadTexture("./resources/Mapa/Mapa_vazio.png");
 
-    
+    // Variáveis de posicionamento
     int ponto_x_lolo_menu = (screen_width - menu_texture.width)/2 + 45;
     int ponto_y_inic_lolo_menu = (screen_height - menu_texture.height)/2 + 85;
     int desloc_y_lolo_menu = 60;
-    Vector2 position={235,200};
-    
-    
-    
     PONTO lolo_sel_ponto = {ponto_x_lolo_menu, ponto_y_inic_lolo_menu};
+    Vector2 position11 = {275, 265};
+    Vector2 position12 = {260, 320};
+    Vector2 position2 = {250, 450};
+    Vector2 position3 = {210, 500};
+    Rectangle textBox = { 255, 385, 300, 50 };
+    Vector2 position4 = {textBox.x + 5, textBox.y + 8};
+    
+    // Declaração das variáveis da caixa de texto com o nome do jogador
+    char name[MAX_INPUT_CHARS + 1] = "\0";
+    int letterCount = 0;
     
     // Main game loop
     while (!WindowShouldClose() && strcmp(status_jogo.parte, "SAIR") != 0)    // Detect window close button or ESC key
@@ -106,7 +123,7 @@ int main(void)
             
             if (IsKeyPressed(KEY_ENTER)) {
                 if (lolo_sel_ponto.y == ponto_y_inic_lolo_menu) {
-                    strcpy(status_jogo.parte, "GAME");
+                    strcpy(status_jogo.parte, "NAME");
                 }
                 else if (lolo_sel_ponto.y == ponto_y_inic_lolo_menu + 1*desloc_y_lolo_menu) {
                     strcpy(status_jogo.parte, "LOAD");
@@ -130,13 +147,58 @@ int main(void)
             EndDrawing();
         }
         
-        if (strcmp(status_jogo.parte, "GAME") == 0) {
+        if (strcmp(status_jogo.parte, "NAME") == 0) {
+            
+            //Implementação da caixa de texto com o nome do jogador
+            // Pega a tecla apertada
+            int key = GetCharPressed();
+
+            // Checa se mais de um caracter foi precionado por frame e adiciona o valor ao nome
+            while (key > 0) {
+                if ((key >= 32) && (key <= 125) && (letterCount < MAX_INPUT_CHARS)) {
+                    name[letterCount] = (char)key;
+                    letterCount++;
+                }
+                
+                key = GetCharPressed();  // Pega o próximo caracter se houver
+            }
+            
+            // Checa se foi apertado o delete e nesse caso diminui o tamanho da string
+            if (IsKeyPressed(KEY_BACKSPACE)) {
+                if (letterCount > 0) {
+                    letterCount--;
+                };
+                    
+                name[letterCount] = '\0';
+            }
+            
+            // Checa se foi apertado enter, nesse caso salva o nome do jogador se ele tiver colocado um e começa o jogo
+            if (IsKeyPressed(KEY_ENTER)) {
+                if (letterCount > 0) {
+                    GRAVACAO jogo_atual;
+                    strcpy(jogo_atual.nomejogador, name);
+                    strcpy(status_jogo.parte, "GAME");
+                };
+            }
+            
             BeginDrawing();
-                
-                DrawTexture(fundo_texture, (screen_width - fundo_texture.width)/2, (screen_height - fundo_texture.height)/2, WHITE);
-                DrawTextEx(Fonte_principal, "Nome do jogador...", position, 25, 1, BLACK);
-                
+            
                 ClearBackground(BLACK);
+
+                //Imagem de fundo
+                DrawTexture(fundo_texture, (screen_width - fundo_texture.width)/2, (screen_height - fundo_texture.height)/2, WHITE);
+                
+                // Texto a cima
+                DrawTextEx(Fonte_principal, "Nome do", position11, 50, 1, BLACK);
+                DrawTextEx(Fonte_principal, "jogador:", position12, 50, 1, BLACK);
+
+                // Caixa para inserir o nome
+                DrawRectangleLines(textBox.x, textBox.y, textBox.width, textBox.height, BLACK);
+                DrawTextEx(Fonte_principal, name, position4, 40, 1, BLACK);
+
+                // Texto a baixo
+                DrawTextEx(Fonte_principal, TextFormat("Caracteres %i de %i", letterCount, MAX_INPUT_CHARS), position2, 25, 1, BLACK);
+                DrawTextEx(Fonte_principal, "Precione Enter para salvar", position3, 20, 1, BLACK);
                    
             EndDrawing();
         }
@@ -146,6 +208,15 @@ int main(void)
             
                 ClearBackground(BLACK);
                 DrawTexture(fundo_texture, (screen_width - fundo_texture.width)/2, (screen_height - fundo_texture.height)/2, WHITE);
+                
+            EndDrawing();
+        }
+        
+        if (strcmp(status_jogo.parte, "GAME") == 0) {
+            BeginDrawing();
+            
+                ClearBackground(BLACK);
+                DrawTexture(mapa_vazio_texture, (screen_width - mapa_vazio_texture.width)/2, (screen_height - mapa_vazio_texture.height)/2, WHITE);
                 
             EndDrawing();
         }
