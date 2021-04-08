2c8ec62d0dffbe9cfb3dd5e5e20b02c7e6df0f5a
FernandoWelzel <66530874+FernandoWelzel@users.noreply.github.com>
2021-04-08 19:29:24 -0300

    Arquivos de gravação e comparação de nomes

--- text.c
+++ text.c
@@ -1,8 +1,10 @@
-bool IsAnyKeyPressed() {
-    bool keyPressed = false;
+// Funções relacionadas a manipulação de texto e entrada do usuário
+
+int IsAnyKeyPressed() {
+    int keyPressed = 0;
     int key = GetKeyPressed();
 
-    if ((key >= 32) && (key <= 126)) keyPressed = true;
+    if ((key >= 32) && (key <= 126)) keyPressed = 1;
 
     return keyPressed;
 }
